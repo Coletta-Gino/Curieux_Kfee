4433cb307f320fdd747a498822394cb45a10c395
Coletta-Gino <gino3869.co@hotmail.com>
2021-12-21 18:28:31 +0100

    Update Of Events Div + Back-Office WordPress

--- content/themes/curieuxkfee/template-parts/header/events.php
+++ content/themes/curieuxkfee/template-parts/header/events.php
@@ -1,5 +1,14 @@
   <!-- Events/Promotions -->
-  <div class="special-event finished">
+  <?php 
+    $field = the_field('status');
+    if ($field == 'terminé') {
+      $value = 'finished';
+    } else {
+      $value = '';
+    }
+  ?>
+
+  <div class="special-event <?php echo $value; ?>">
     <a href="#"><i class="fa fa-pause-circle-o fa-2x" aria-hidden="true"></i></a>
 
     <div class="special-event__items offer">
@@ -14,9 +23,14 @@
       ?>
 
       <?php if ($wpqueryArticles->have_posts()): while ($wpqueryArticles->have_posts()): $wpqueryArticles->the_post(); ?>
-        <strong>20% de réduction</strong>
-        <i class="fa fa-coffee" aria-hidden="true"></i>
-        <span>Jusqu'au 05/12</span>
+        <strong><?php the_field('name'); ?></strong>
+        <i class="fa <?php the_field('icon'); ?>" aria-hidden="true"></i>
+        <span>
+          <p><?php the_field('period_beginning'); ?></p>
+          <p><?php the_field('date_beginning'); ?></p>
+          <p><?php the_field('period_ending') ?></p>
+          <p><?php the_field('date_ending'); ?></p>
+        </span>
       <?php endwhile; endif; ?>
       </div>
     </div>
