36dff885fb26eeec16ca5f883a753b3b2ebea9a6
Coletta-Gino <gino3869.co@hotmail.com>
2022-03-28 19:28:28 +0200

    Last Details (Part. 1)

--- content/themes/curieuxkfee/template-parts/footer/simplified-logo.php
+++ content/themes/curieuxkfee/template-parts/footer/simplified-logo.php
@@ -1,9 +1,11 @@
     <!-- Logo (Simplified) -->
     <div class="simplified-logo">
-      <?php 
-        $image_attributes = wp_get_attachment_image_src($attachment_id = 125);
-        $alt_text = get_post_meta(125, '_wp_attachment_image_alt', true);
-      ?>
+      <div>
+        <?php 
+          $image_attributes = wp_get_attachment_image_src($attachment_id = 167);
+          $alt_text = get_post_meta(167, '_wp_attachment_image_alt', true);
+        ?>
   
-      <img src="<?php echo $image_attributes[0]; ?>" alt="<?php echo $alt_text; ?>" />
+        <img src="<?php echo $image_attributes[0]; ?>" alt="<?php echo $alt_text; ?>" />
+      </div>
     </div>
