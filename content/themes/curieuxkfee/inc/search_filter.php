7e7ca06319c4596b82997d23263f975a800c46bf
Coletta-Gino <gino3869.co@hotmail.com>
2022-09-22 17:42:06 +0200

    Update Search Filters + Update Of Webpack

--- content/themes/curieuxkfee/inc/search_filter.php
+++ content/themes/curieuxkfee/inc/search_filter.php
@@ -6,8 +6,8 @@ function my_search_filter($query) {
     // TODO => be carreful to change the values of the id of all categories !!!
     if ($query->is_search) {
       // $query->set('post_type', 'pages'); // to exclude pages
-      $query->set('cat', array(-18, -19)); // to exclude children of 'ephemeral' and 'permanent' categories
-      $query->set('category__not_in', array(1, 16, 11, 24, 41)); // to exclude 'uncategorized', 'header', 'home', 'informations', and 'footer' articles
+      $query->set('cat', array(-8, -9)); // to exclude children of 'ephemeral' and 'permanent' categories
+      $query->set('category__not_in', array(1, 10, 3, 14, 18)); // to exclude 'uncategorized', 'header', 'home', 'informations', and 'footer' articles
     }
 
     return $query;
